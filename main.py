67d206048da4930a0e27b909b86e90565057f5a1
LexaAsin <147340113+LexaAsin@users.noreply.github.com>
2024-05-19 21:43:05 +0300

    Новая фича
    Реализованы статусы. Теперь с помощью тг бота можно указывать и изменять прогресс каждой задачи.

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ from telebot import types
 bot = telebot.TeleBot('7123961112:AAEM7EvLW9PAiVKge_tCWSUlmCjQYE4HU5I')
 
 jira_options = {'server': 'https://pi311pract.atlassian.net'}
-jira = JIRA("https://pi311pract.atlassian.net", basic_auth=("lasin470@gmail.com", "ATATT3xFfGF0kZqyBEFyOfixpslfLErNKziXeuurRlvulZ6w2z4RfVRvAhppwrteVN1lvZ0tzdfT9rIBsfej3-Bh2MESUXJeEvxK28gM8IEJowxJimXMTxZjKc1-cnWPPXfqo-N-2EK9T5pv87zj9v8VnVHk5t40aPBFtU1r3aQhHP7mOCbqITg=3FD6BE2A"))
-
+jira = JIRA("https://pi311pract.atlassian.net", basic_auth=("lasin470@gmail.com", "ATATT3xFfGF02B-2MCvoEgPMl4xZek9T56Mh6Falg5VeZGpJhu2AMa7zfaiHSmBFNv6Y-7XtITfC4Sl-2aaIq9L2GsiNcn9gtjv_LTbxez3rTZL8MBgSOynaomg6wTq6aaWplLWli2X22fJR8WQDXk4JUj1YSgqL5jFO9qt_f096hwEziWmZzUo=855B362D"))
 
 @bot.message_handler(commands=["start"])
 def bot_messages(message):
@@ -17,11 +16,12 @@ def bot_messages(message):
     markup.add(item2)
     item3 = types.KeyboardButton("Открыть задачу")
     markup.add(item3)
-    item4 = types.KeyboardButton("Удалить задачу")
+    item4 = types.KeyboardButton("Изменить статус")
     markup.add(item4)
+    item5 = types.KeyboardButton("Удалить задачу")
+    markup.add(item5)
 
-    bot.send_message(message.chat.id, 'Я могу исполнить три желания:\n Создать задачу\n Найти задачу\n Открыть задачу\nУдалить задачу', reply_markup=markup)
-
+    bot.send_message(message.chat.id, 'Я могу исполнить 5 желаний:\n Создать задачу\n Найти задачу\n Открыть задачу\n Изменить статус\n Удалить задачу', reply_markup=markup)
 
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
@@ -29,23 +29,30 @@ def handle_text(message):
         bot.send_message(message.chat.id, 'Создание задачи. Введите название задачи.')
         bot.register_next_step_handler(message, get_create_issue_summary)
     elif message.text.strip() == 'Найти задачу':
+        bot.send_message(message.chat.id, 'Введите ключевое слово для поиска задач.')
         bot.register_next_step_handler(message, get_search_issue)
     elif message.text.strip() == 'Открыть задачу':
-        bot.send_message(message.chat.id, 'Открытые задачи. Введите номер задачи TJ-...')
+        bot.send_message(message.chat.id, 'Открытые задачи. Введите номер задачи, например KAN-...')
         bot.register_next_step_handler(message, get_open_issue)
     elif message.text.strip() == 'Удалить задачу':
         bot.send_message(message.chat.id, 'Удаление задачи. Введите номер задачи KAN-...')
         bot.register_next_step_handler(message, delete_issue)
-
+    elif message.text.strip() == 'Изменить статус':
+        bot.send_message(message.chat.id, 'Изменение статуса. Введите номер задачи KAN-...')
+        bot.register_next_step_handler(message, change)
 
 #Добавление задачи
 def get_create_issue_summary(message):
     global jira_summary
     jira_summary = message.text.strip()
+    bot.send_message(message.chat.id, 'Создание задачи. Введите Статус.')
+    bot.register_next_step_handler(message, get_create_issue_status)
+
+def get_create_issue_status(message):
+    global jira_status
+    jira_status = message.text.strip()
     bot.send_message(message.chat.id, 'Создание задачи. Введите приоритет.')
     bot.register_next_step_handler(message, get_create_issue_priority)
-
-
 def get_create_issue_priority(message):
     global jira_priority
     jira_priority = message.text.strip()
@@ -56,11 +63,11 @@ def get_create_issue_description(message):
     global jira_description
     jira_description = message.text.strip()
     bot.send_message(message.chat.id, 'Название: ' + jira_summary + '\n' +
+                                      'Статус: ' + jira_priority + '\n' +
                                       'Приоритет: ' + jira_priority + '\n' +
                                       'Описание: ' + jira_description)
     bot.send_message(message.chat.id, 'Создать задачу? (Да/Нет)')
     bot.register_next_step_handler(message, get_create_issue)
-
 
 def get_create_issue(message):
     if message.text.strip() == 'Да' :
@@ -68,11 +75,16 @@ def get_create_issue(message):
                                       summary= jira_summary,
                                       description = jira_description,
                                       issuetype= {'name': 'Task'},
-                                      priority={'name': jira_priority})
+                                      priority = {'name': jira_priority})
         bot.send_message(message.chat.id, 'Задача ' + issue.key + ' создана')
-    elif message.text.strip() == 'Нет' :
+        if jira_status.upper() == 'TO DO':
+            jira.transition_issue(issue.key, '11')
+        elif jira_status.upper() == 'IN PROGRESS':
+            jira.transition_issue(issue.key, '21')
+        elif jira_status.upper() == 'DONE':
+            jira.transition_issue(issue.key, '31')
+    elif message.text.strip() == 'Нет':
         bot.send_message(message.chat.id, 'Создание задачи отменено.')
-
 
 #Поиск задачи
 def get_search_issue(message):
@@ -83,7 +95,6 @@ def get_search_issue(message):
     for issue in issues:
         ans.append(issue.key)
     bot.send_message(message.chat.id, 'Найдены задачи:' + '\n'.join(ans))
-
 
 #Окрытие задачи
 def get_open_issue(message):
@@ -101,6 +112,44 @@ def get_open_issue(message):
     else:
         bot.send_message(message.chat.id, 'Некорректный формат ключа задачи. Введите ключ в формате KAN-123.')
 
+#Изменение статуса
+def change(message):
+    global issue_key
+    issue_key = message.text.strip().upper()
+
+    if issue_key.startswith('KAN-'):
+        try:
+            issue = jira.issue(issue_key)
+            current_status = issue.fields.status.name
+            bot.send_message(message.chat.id, f'Текущий статус задачи "{issue_key}": {current_status}')
+            bot.send_message(message.chat.id, 'Введите статус, в который хотите перевести задачу: "TO DO","IN PROGRESS","DONE"')
+            bot.register_next_step_handler(message, change_status)
+        except Exception as e:
+            bot.send_message(message.chat.id, 'Ошибка при получении статуса задачи: ' + str(e))
+    else:
+        bot.send_message(message.chat.id, 'Некорректный формат ключа задачи. Введите ключ в формате KAN-123.')
+
+
+def change_status(message):
+    change_s = message.text.strip().upper()
+    if issue_key.startswith('KAN-'):
+        try:
+            issue = jira.issue(issue_key)
+            if change_s == "TO DO":
+                jira.transition_issue(issue.key, '11')
+                bot.send_message(message.chat.id, 'Статус изменен.')
+            elif change_s == "IN PROGRESS":
+                jira.transition_issue(issue.key, '21')
+                bot.send_message(message.chat.id, 'Статус изменен.')
+            elif change_s == "DONE":
+                jira.transition_issue(issue.key, '31')
+                bot.send_message(message.chat.id, 'Статус изменен.')
+            else:
+                bot.send_message(message.chat.id, 'Некорректный статус. Введите один из: TO DO, IN PROGRESS, DONE')
+        except Exception as e:
+            bot.send_message(message.chat.id, 'Ошибка при изменении статуса: ' + str(e))
+    else:
+        bot.send_message(message.chat.id, 'Некорректный формат ключа задачи. Введите ключ в формате KAN-123.')
 
 #Удаление задачи
 def delete_issue(message):
@@ -114,7 +163,6 @@ def delete_issue(message):
             bot.send_message(message.chat.id, 'Ошибка при удалении задачи: ' + str(e))
     else:
         bot.send_message(message.chat.id, 'Некорректный формат ключа задачи. Введите ключ в формате KAN-123.')
-
 
 if __name__ == '__main__':
     bot.infinity_polling()
